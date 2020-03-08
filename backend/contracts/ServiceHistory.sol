80918cccbbf16fd8ab047ff8ccb05e7ca111bc27
Sergey Shevchuk <serhey.shevchuk@gmail.com>
2020-03-08 18:18:15 -0400

    added error messages to the contract

--- backend/contracts/ServiceHistory.sol
+++ backend/contracts/ServiceHistory.sol
@@ -2,29 +2,34 @@ pragma solidity >=0.4.22 <0.7.0;
 
 contract ServiceHistory {
     address public owner = msg.sender;
-    
-    mapping (string => string) public vinData;
-    mapping (address => bool) public dealers;
-    
-    event Update(string vin, string data);
-    
-    modifier ownerOnly() {
-      require(msg.sender == owner);
-      _; 
-    }
 
+    mapping(string => string) public vinData;
+    mapping(address => bool) public dealers;
+
+    event Update(string vin, string data);
+
+    modifier ownerOnly() {
+        require(
+            msg.sender == owner,
+            "You're not authorised to call this funciton"
+        );
+        _;
+    }
 
     function getData(string memory vin) public view returns (string memory) {
         return vinData[vin];
     }
-    
+
     function updateData(string memory vin, string memory data) public {
-        require(dealers[msg.sender]);
+        require(
+            dealers[msg.sender],
+            "You must be an authorized dealer to update this data"
+        );
         vinData[vin] = data;
         emit Update(vin, data);
     }
-    
-    function addDealer (address dealer) public ownerOnly {
+
+    function addDealer(address dealer) public ownerOnly {
         dealers[dealer] = true;
     }
 
